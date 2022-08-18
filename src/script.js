b370cb4c9e9714cba770eb90160455ca986c02d9
emefaAngela <63966516+emefaAngela@users.noreply.github.com>
2022-08-18 22:36:49 +0000

    adding eventlisteners

--- src/script.js
+++ src/script.js
@@ -1,11 +1,15 @@
 var bill =document.getElementById("bill-amount");
-var customtip =document.getElementById("custom_tip");
 var people =document.getElementById("no_of_people");
 var tip_per_person =document.getElementById("tip_person");
 var total_per_person =document.getElementById("total_person");
 var reset =document.getElementById("reset");
 var tips=document.querySelectorAll("row-1");
 
+people.addEventListener("input",peopleInput);
+bill.addEventListener("input",billInput);
+tips.forEach(function(val){
+val.addEventListener("click",handleclick);
+});
 function handleclick(event){
 tips.forEach(function(val){
     val.classList.remove(active_tip);
